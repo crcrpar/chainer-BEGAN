3719eea8f5189bebaedf3d563aedc0ae283408e5
crcrpar <masakikozuki@Masaki-no-MacBook-Pro.local>
2017-04-03 15:37:49 +0900

    check shape

--- models/encoder.py
+++ models/encoder.py
@@ -16,10 +16,10 @@ class Encoder(chainer.Chain):
         args = {
                 'ksize': 3,
                 'stride': 1,
-                'pad': 2,
+                'pad': 1,
                 'initialW': chainer.initializers.HeNormal()
                 }
-        self._layers = { 
+        self._layers = {
                 'conv_0': L.Convolution2D(3, n, **args),
                 'conv_1': L.Convolution2D(n, n, **args),
                 'conv_2': L.Convolution2D(n, 2 * n, **args),
@@ -55,6 +55,27 @@ class Encoder(chainer.Chain):
         embedded = self.fc(h8)
         return embedded
 
+    def validate(self, x):
+        if not isinstance(x, chainer.Variable):
+            x = chainer.Variable(x)
+        h0 = F.elu(self.conv_0(x))
+        h0_ss = F.average_pooling_2d(h0, 2)
+        h1 = F.elu(self.conv_1(h0_ss))
+        h2 = F.elu(self.conv_2(h1))
+        h2_ss = F.average_pooling_2d(h2, 2)
+        h3 = F.elu(self.conv_3(h2_ss))
+        h4 = F.elu(self.conv_4(h3))
+        h4_ss = F.average_pooling_2d(h4, 2)
+        h5 = F.elu(self.conv_5(h4_ss))
+        h6 = F.elu(self.conv_6(h5))
+        h6_ss = F.average_pooling_2d(h6, 2)
+        h7 = F.elu(self.conv_7(h6_ss))
+        h8 = F.elu(self.conv_8(h7))
+        embedded = self.fc(h8)
+        print(h0.shape, h0_ss.shape, h1.shape, h2.shape, h2_ss.shape,
+        h3.shape, h4.shape, h4_ss.shape, h5.shape, h6.shape, h6_ss.shape,
+        h7.shape, h8.shape)
+
     @property
     def train(self):
         return self._train
@@ -71,5 +92,6 @@ if __name__ == '__main__':
     encoder = Encoder(16, 100)
     shape = (bs, 3, 64, 64)
     x = np.random.normal(size=shape).astype(np.float32)
+    encoder.validate(x)
     out = encoder(x)
     print('# out.shape: {}'.format(out.shape))
